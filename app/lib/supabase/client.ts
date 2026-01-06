5ebf442c6858a707e6670d3e1ee5ab61f723f3de
Adam Bailey <ajbailey564@gmail.com>
2026-01-05 19:59:47 -0500

    lets try this

--- app/lib/supabase/client.ts
+++ app/lib/supabase/client.ts
@@ -1,13 +1,13 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
 
-// if (!supabaseUrl || !supabaseAnonKey) {
-//   throw new Error(
-//     "Missing Supabase environment variables. Please check your .env file."
-//   );
-// }
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error(
+    "Missing Supabase environment variables. Please check your .env file."
+  );
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
